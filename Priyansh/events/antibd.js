e865d76f525b8f4d511e341c970570d8cd2ed22e
RAJPUT-8090 <bdada5990@gmail.com>
2025-04-25 12:13:06 +0530

    Update antibd.js

--- Priyansh/events/antibd.js
+++ Priyansh/events/antibd.js
@@ -1,20 +1,33 @@
 module.exports.config = {
   name: "antibd",
-  eventType: ["log:user-nickname"],
-  version: "0.0.1",
+  eventType: ["log:user-nickname", "log:thread-name"],
+  version: "0.0.2",
   credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-  description: "Against changing Bot's nickname"
+  description: "बोट और ग्रुप का नाम केवल एडमिन ही बदल सकें"
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
-    var { logMessageData, threadID, author } = event;
-    var botID = api.getCurrentUserID();
-    var { BOTNAME, ADMINBOT } = global.config;
-    var { nickname } = await Threads.getData(threadID, botID);
-    var nickname = nickname ? nickname : BOTNAME;
-    if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
-        api.changeNickname(nickname, threadID, botID)
-        var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - 𝙏𝙐𝙈 𝘽𝙊𝙏 𝙆𝘼 𝙉𝙄𝘾𝙆𝙉𝘼𝙈𝙀 𝘾𝙃𝘼𝙉𝙂𝙀 𝙉𝙄 𝙆𝘼𝙍 𝙎𝘼𝙆𝙏𝘼𝙔 😹🖐`}, threadID);
-    }  
-        }
+  const { logMessageData, threadID, author } = event;
+  const botID = api.getCurrentUserID();
+  const { BOTNAME, ADMINBOT } = global.config;
+
+
+  const { nickname } = await Threads.getData(threadID, botID);
+  const currentNickname = nickname || BOTNAME;
+  const { threadName } = await Threads.getData(threadID);
+  const currentThreadName = threadName;
+
+
+  if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != currentNickname) {
+    api.changeNickname(currentNickname, threadID, botID);
+    const userData = await Users.getData(author);
+    return api.sendMessage({ body: `${userData.name} - 𝙏𝙐𝙈 𝘽𝙊𝙏 𝙆𝘼 𝙉𝙄𝘾𝙆𝙉𝘼𝙈𝙀 𝘾𝙃𝘼𝙉𝙂𝙀 𝙉𝙄 𝙆𝘼𝙍 𝙎𝘼𝙆𝙏𝘼𝙔 😹🖐` }, threadID);
+  }
+
+
+  if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.thread_name != currentThreadName) {
+    api.setTitle(currentThreadName, threadID);
+    const userData = await Users.getData(author);
+    return api.sendMessage({ body: `${userData.name} - 𝙏𝙐𝙈 𝙂𝙍𝙐𝙋 𝙆𝘼 𝙉𝘼𝙈𝙀 𝘾𝙃𝘼𝙉𝙂𝙀 𝙉𝙄 𝙆𝘼𝙍 𝙎𝘼𝙆𝙏𝘼𝙔 😹🖐` }, threadID);
+  }
+};
